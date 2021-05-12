d8a63bb4334a9b50fefd70933e9389dba9659404
Melih Çiçek <melihcicek@gmail.com>
2021-05-12 19:02:09 +0300

    parameter type changed to universal reference for move

--- CppExercise/Allocator.h
+++ CppExercise/Allocator.h
@@ -29,7 +29,7 @@ namespace mc {
 		void deallocate(pointer, std::size_t) {	}
 
 		template<typename T, typename ...Args>
-		void construct(T* t, Args ...args)
+		void construct(T* t, Args&& ...args)
 		{
 			new (t) T(std::forward<Args>(args)...);
 		}
