b637219c7e2d3b54cf5a4689f0568ec6ad783933
Melih Çiçek <melihcicek@gmail.com>
2021-07-15 17:49:35 +0300

    Update FactoryClass.h

--- CppExercise/FactoryClass.h
+++ CppExercise/FactoryClass.h
@@ -3,14 +3,15 @@
 #include <map>
 #include <memory>
 #include <functional>
+#include <string>
 
 namespace mc {
     template<typename T>
     class general_factory {
-        std::map<string, std::function<std::unique_ptr<T>()>> m_fac_members;
+        std::map<std::string, std::function<std::unique_ptr<T>()>> m_fac_members;
     public:
 
-        unique_ptr<T> get_instance(string name)
+        std::unique_ptr<T> get_instance(std::string name)
         {
             if (auto iter = m_fac_members.find(name); iter != m_fac_members.end())
                 return iter->second();
@@ -19,7 +20,7 @@ namespace mc {
         }
 
         template<typename T, typename ...Args>
-        void register_new_type(string name, Args&& ...args)
+        void register_new_type(std::string name, Args&& ...args)
         {
             m_fac_members.insert({ name,[...args = std::forward<Args>(args)]() {  return std::make_unique<T>(args...); } });
         }
